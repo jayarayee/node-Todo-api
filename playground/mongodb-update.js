23753f29d5feb2252c47129808b6e30475510da6
Jai <rayeejaya@gmail.com>
2018-02-18 22:11:10 -0500

    add updat file

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -11,29 +11,29 @@ MongoClint.connect('mongodb://localhost:27017/TodoApp', (err, clint)=>{
     console.log('Connected to MongoDB server');
     const db = clint.db('TodoApp')
 
-    // db.collection('Todos').findOneAndUpdate({
-    //     _id: new ObjectID('5a89fc849c981684df2efc6e')
-    // }, {
-    //     $set: {
-    //         completed: true
-    //     }
-    // },{
-    //     returnOriginal: false  
-    // }).then((result) => {
-    //     console.log(result);
-    // })
-    db.collection('Mydata').findOneAndUpdate({
-        _id: new ObjectID('5a89ee621b8208f6b3f6df69')
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID('5a89fc849c981684df2efc6e')
     }, {
         $set: {
-            name: 'Andrew'
+            completed: true
         }
-    },{
-        $inc: 1
     },{
         returnOriginal: false  
     }).then((result) => {
         console.log(result);
-    });
+    })
+    // db.collection('Mydata').findOneAndUpdate({
+    //     _id: new ObjectID('5a89ee621b8208f6b3f6df69')
+    // }, {
+    //     $set: {
+    //         name: 'Andrew'
+    //     }
+    // },{
+    //     $inc: 1
+    // },{
+    //     returnOriginal: false  
+    // }).then((result) => {
+    //     console.log(result);
+    // });
     // clint.close();
 });
